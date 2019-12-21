6f645607ba30b4a1fb091ddb9f7a925b4b491a30
Jonas-hcr <programeicafe@outlook.com>
2019-12-21 17:26:34 -0300

    Missao Concluída, General!

---
+++ missao-estelar/primeira-missao/jonasHcr/js/js.js
@@ -0,0 +1,12 @@
+console.log('Missão estelar JS com JS');
+
+function eventoOnload(){
+    console.log('Evento OnLoad disparado!');
+}
+
+function eventoOnclick(){
+    console.log('Missão Estelar JS realizada com sucesso!!');
+}
+
+
+var missao = "Missão Estelar JS em execução!!!";
